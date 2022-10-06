5709e52340201e1b4f4288db1111f5e2c6ba7645
Arthur <arthur@utopios.solutions>
2022-10-06 14:11:02 +0200

    tsconfig

---
+++ typescript/basic.ts
@@ -0,0 +1,6 @@
+const lowerString = (arg: string): string => {
+    return arg.toLowerCase();
+}
+
+console.log(lowerString("bonjour"));
+//console.log(lowerString(22));
---
+++ typescript/js/basic.js
@@ -0,0 +1,5 @@
+var lowerString = function (arg) {
+    return arg.toLowerCase();
+};
+console.log(lowerString("bonjour"));
+//console.log(lowerString(22));
