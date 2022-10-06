241fd4c41a0b2806b90bc39f88be70f6387995d6
Arthur <arthur@utopios.solutions>
2022-10-06 14:30:41 +0200

    exercise typescript

--- typescript/basic.ts
+++ typescript/basic.ts
@@ -4,3 +4,19 @@ const lowerString = (arg: string): string => {
 
 console.log(lowerString("bonjour"));
 //console.log(lowerString(22));
+
+let toto: string = "Toto";
+let isOk: boolean = true;
+let myNumber: number = 23;
+
+let firstArray: string[] = [];
+firstArray.push("hello");
+
+let secondArray = [12, 23];
+
+let myTuple: [string, number] = ["one", 24];
+
+let myObject: object = {
+    name: "toto",
+    age: 23
+}
--- typescript/js/basic.js
+++ typescript/js/basic.js
@@ -1,5 +1,14 @@
-var lowerString = function (arg) {
+const lowerString = (arg) => {
     return arg.toLowerCase();
 };
 console.log(lowerString("bonjour"));
 //console.log(lowerString(22));
+let toto = "Toto";
+let isOk = true;
+let myNumber = 23;
+let firstArray = [];
+firstArray.push("hello");
+let secondArray = [12, 23];
+let myTuple = ["one", 24];
+myTuple.push("hello");
+console.log(myTuple);
